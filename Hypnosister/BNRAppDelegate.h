dd355188691162f1176a6b1a01b6f5e6e2a500e9
Jan Graßegger <jan@anycook.de>
2014-04-06 00:00:39 +0200

    Initial Commit

---
+++ Hypnosister/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Hypnosister
+//
+//  Created by Jan Grassegger on 06.04.14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
